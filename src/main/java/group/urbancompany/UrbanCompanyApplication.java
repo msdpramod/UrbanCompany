a734755141887fbbf504a4dddcdd16f391b75f75
kuppapramod <kuppapramod@gmail.com>
2025-10-19 07:19:09 +0530

    Hi

---
+++ src/main/java/group/urbancompany/UrbanCompanyApplication.java
@@ -0,0 +1,13 @@
+package group.urbancompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanCompanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrbanCompanyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/group/urbancompany/UrbanCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package group.urbancompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanCompanyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
