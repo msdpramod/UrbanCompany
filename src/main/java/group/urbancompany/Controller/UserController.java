6c34e1bf3c19c38a2c2a8cd91f40de2dc568ea4b
kuppapramod <kuppapramod@gmail.com>
2025-10-19 08:44:11 +0530

    added User service

--- src/main/java/group/urbancompany/Controller/UserController.java
+++ src/main/java/group/urbancompany/Controller/UserController.java
@@ -2,6 +2,7 @@ package group.urbancompany.Controller;
 
 
 import group.urbancompany.Models.User;
+import group.urbancompany.Service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,17 +13,21 @@ import java.util.List;
 
 @RestController
 public class UserController {
-    private List<User> users = new ArrayList<>();
+   private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/api/users")
     public List<User> getAllUsers() {
         // Logic to retrieve all users
-        return users;
+        return userService.getUsers();
     }
 
     @PostMapping("/api/users")
-    public List<User> createUser(@RequestBody User user) {
-        users.add(user);
-        return users;
+    public String createUser(@RequestBody User user) {
+       userService.addUser(user);
+         return "User added successfully";
     }
 }
